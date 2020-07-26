e4f143b0d07b91809e8a56a3faf155e548eba630
venugopalreddy123 <venu641995@gmail.com>
2020-07-26 08:57:13 +0530

    fist followups

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { Grid } from "@material-ui/core";
+import youtube from "./Api/Youtube";
 
-function App() {
-  return <div className="App"> hello world</div>;
+import { SearchBar, VideoDetails } from "./Components";
+
+class App extends React.Component {
+  state = {
+    video: [],
+    selectedVideo: null,
+  };
+  handleSubmit = async (searchTerm) => {
+    const response = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyAQXgqu1aGD6FhT3Q0OwIbLX3tAKKdfcYQ",
+        q: searchTerm,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+  render() {
+    const { selectedVideo } = this.state;
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={12}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+              <Grid item xs={8}>
+                <VideoDetails video={this.state.selectedVideo} />
+                <Grid item xs={4}>
+                  {/* Video list*/}
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
-
 export default App;
---
+++ src/Components/VideoList.js
@@ -0,0 +1,7 @@
+import React from "react";
+class VideoList extends React.Component {
+  render() {
+    return <h1> Video list </h1>;
+  }
+}
+export default VideoList;
